b54d6e7ffe0a4f353610fd4fcb640f5eaf132b85
ywang1724 <kobebryant42536@gmail.com>
2014-12-25 17:24:32 +0800

    add admin

--- app/controllers/articles.server.controller.js
+++ app/controllers/articles.server.controller.js
@@ -73,15 +73,27 @@ exports.delete = function(req, res) {
  * List of Articles
  */
 exports.list = function(req, res) {
-	Article.find().sort('-created').populate('user', 'displayName').exec(function(err, articles) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.json(articles);
-		}
-	});
+	if (req.user.roles[0] === 'admin') {
+		Article.find().sort('-created').populate('user', 'displayName').exec(function(err, articles) {
+			if (err) {
+				return res.status(400).send({
+					message: errorHandler.getErrorMessage(err)
+				});
+			} else {
+				res.json(articles);
+			}
+		});
+	} else {
+		Article.find({ user: req.user.id}).sort('-created').populate('user', 'displayName').exec(function(err, articles) {
+			if (err) {
+				return res.status(400).send({
+					message: errorHandler.getErrorMessage(err)
+				});
+			} else {
+				res.json(articles);
+			}
+		});
+	}
 };
 
 /**
