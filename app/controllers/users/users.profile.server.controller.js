23a5f4e4e96ab2d3cbccb4d37fcc875b0d009ae8
ywang1724 <kobebryant42536@gmail.com>
2015-01-04 16:43:23 +0800

    fix admin manage users list bugs

--- app/controllers/users/users.profile.server.controller.js
+++ app/controllers/users/users.profile.server.controller.js
@@ -4,74 +4,99 @@
  * Module dependencies.
  */
 var _ = require('lodash'),
-	errorHandler = require('../errors.server.controller.js'),
-	mongoose = require('mongoose'),
-	passport = require('passport'),
-	User = mongoose.model('User');
+    errorHandler = require('../errors.server.controller.js'),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    User = mongoose.model('User');
 
 /**
  * Update user details
  */
-exports.update = function(req, res) {
-	// Init Variables
-	var user = req.user;
-	var message = null;
+exports.update = function (req, res) {
+    // Init Variables
+    var user = req.user;
+    var message = null;
 
-	// For security measurement we remove the roles from the req.body object
-	delete req.body.roles;
-	if (user.roles[0] === 'admin') {
-		user = User.findById(req.body._id);
-	} else {
-		delete req.body.isActive;
-	}
+    // For security measurement we remove the roles from the req.body object
+    delete req.body.roles;
 
-	if (user) {
-		// Merge existing user
-		user = _.extend(user, req.body);
-		user.updated = Date.now();
+    if (user) {
+        if (user.roles[0] === 'admin' && user._id.toString() !== req.body._id) {
+            User.findById(req.body._id, function (err, obj) {
+                if (err) {
+                    return res.status(400).send({
+                        message: errorHandler.getErrorMessage(err)
+                    });
+                } else {
+                    user = _.extend(obj, req.body);
+                    user.updated = Date.now();
 
-		user.save(function(err) {
-			if (err) {
-				return res.status(400).send({
-					message: errorHandler.getErrorMessage(err)
-				});
-			} else {
-				req.login(user, function(err) {
-					if (err) {
-						res.status(400).send(err);
-					} else {
-						res.json(user);
-					}
-				});
-			}
-		});
-	} else {
-		res.status(400).send({
-			message: 'User is not signed in'
-		});
-	}
+                    user.save(function (err) {
+                        if (err) {
+                            return res.status(400).send({
+                                message: errorHandler.getErrorMessage(err)
+                            });
+                        } else {
+                            req.login(req.user, function (err) {
+                                if (err) {
+                                    res.status(400).send(err);
+                                } else {
+                                    res.json(user);
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        } else {
+            delete req.body.isActive;
+            // Merge existing user
+            user = _.extend(user, req.body);
+            user.updated = Date.now();
+
+            user.save(function (err) {
+                if (err) {
+                    return res.status(400).send({
+                        message: errorHandler.getErrorMessage(err)
+                    });
+                } else {
+                    req.login(user, function (err) {
+                        if (err) {
+                            res.status(400).send(err);
+                        } else {
+                            res.json(user);
+                        }
+                    });
+                }
+            });
+        }
+    } else {
+        res.status(400).send({
+            message: 'User is not signed in'
+        });
+    }
 };
 
 /**
  * Send User
  */
-exports.me = function(req, res) {
-	res.json(req.user || null);
+exports.me = function (req, res) {
+    res.json(req.user || null);
 };
 
 /**
  * List of Users
  */
-exports.list = function(req, res) {
-	if (req.user.roles[0] === 'admin') {
-		User.find().exec(function(err, users) {
-			if (err) {
-				return res.status(400).send({
-					message: errorHandler.getErrorMessage(err)
-				});
-			} else {
-				res.json(users);
-			}
-		});
-	}
+exports.list = function (req, res) {
+    if (req.user.roles[0] === 'admin') {
+        User.find().exec(function (err, users) {
+            if (err) {
+                return res.status(400).send({
+                    message: errorHandler.getErrorMessage(err)
+                });
+            } else {
+                res.json(users);
+            }
+        });
+    }
 };
