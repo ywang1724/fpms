d62c290854cf26d7d84af31dd4c1164169b3a309
ywang1724 <kobebryant42536@gmail.com>
2015-01-15 16:51:03 +0800

    modify reference script string

--- public/modules/apps/controllers/apps.client.controller.js
+++ public/modules/apps/controllers/apps.client.controller.js
@@ -10,7 +10,7 @@ angular.module('apps').controller('AppsController', ['$scope', '$stateParams', '
 		$scope.types = ['java', 'node.js'];
 
 		$scope.showName = Authentication.user.roles[0] === 'admin' ? true : false;
-		$scope.script = '<script type="text/javascript" src="http://192.168.88.177:3000/rookie.js"></script>';
+		$scope.script = '<script type="text/javascript" src="http://192.168.88.177:3000/rookie.js/' + $stateParams.appId + '"></script>';
 
 		// Create new App
 		$scope.create = function() {
