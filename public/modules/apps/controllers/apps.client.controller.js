18977d183945ea65ac333794794de305a4551d0a
ywang1724 <kobebryant42536@gmail.com>
2015-01-19 09:41:37 +0800

    fix not authorized bug

--- public/modules/apps/controllers/apps.client.controller.js
+++ public/modules/apps/controllers/apps.client.controller.js
@@ -2,107 +2,109 @@
 
 // Apps controller
 angular.module('apps').controller('AppsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Apps', 'DTOptionsBuilder',
-	function($scope, $stateParams, $location, Authentication, Apps, DTOptionsBuilder) {
-		$scope.authentication = Authentication;
+    function ($scope, $stateParams, $location, Authentication, Apps, DTOptionsBuilder) {
+        $scope.authentication = Authentication;
 
-		//可从后台动态获取数据，以后有时间完成
-		$scope.type = 'java';
-		$scope.types = ['java', 'node.js'];
+        //可从后台动态获取数据，以后有时间完成
+        $scope.type = 'java';
+        $scope.types = ['java', 'node.js'];
 
-		$scope.showName = Authentication.user.roles[0] === 'admin' ? true : false;
-		$scope.script = '<script type="text/javascript" src="http://192.168.88.177:3000/rookie.js/' + $stateParams.appId + '"></script>';
+        if (Authentication.user) {
+            $scope.showName = Authentication.user.roles[0] === 'admin' ? true : false;
+        }
+        $scope.script = '<script type="text/javascript" src="http://192.168.88.177:3000/rookie.js/' + $stateParams.appId + '"></script>';
 
-		// Create new App
-		$scope.create = function() {
-			// Create new App object
-			var app = new Apps ({
-				name: this.name,
-				type: this.type,
-				server: this.server
-			});
+        // Create new App
+        $scope.create = function () {
+            // Create new App object
+            var app = new Apps({
+                name: this.name,
+                type: this.type,
+                server: this.server
+            });
 
-			// Redirect after save
-			app.$save(function(response) {
-				$location.path('apps/' + response._id);
+            // Redirect after save
+            app.$save(function (response) {
+                $location.path('apps/' + response._id);
 
-				// Clear form fields
-				$scope.name = '';
-				$scope.type = 'java';
-				$scope.server = '';
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+                // Clear form fields
+                $scope.name = '';
+                $scope.type = 'java';
+                $scope.server = '';
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-		// Remove existing App
-		$scope.remove = function(app) {
-			if ( app ) { 
-				app.$remove();
+        // Remove existing App
+        $scope.remove = function (app) {
+            if (app) {
+                app.$remove();
 
-				for (var i in $scope.apps) {
-					if ($scope.apps [i] === app) {
-						$scope.apps.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.app.$remove(function() {
-					$location.path('apps');
-				});
-			}
-		};
+                for (var i in $scope.apps) {
+                    if ($scope.apps [i] === app) {
+                        $scope.apps.splice(i, 1);
+                    }
+                }
+            } else {
+                $scope.app.$remove(function () {
+                    $location.path('apps');
+                });
+            }
+        };
 
-		// Update existing App
-		$scope.update = function() {
-			var app = $scope.app;
+        // Update existing App
+        $scope.update = function () {
+            var app = $scope.app;
 
-			app.$update(function() {
-				$location.path('apps/' + app._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+            app.$update(function () {
+                $location.path('apps/' + app._id);
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-		// Find a list of Apps
-		$scope.find = function() {
-			$scope.apps = Apps.query();
-		};
+        // Find a list of Apps
+        $scope.find = function () {
+            $scope.apps = Apps.query();
+        };
 
-		// Find existing App
-		$scope.findOne = function() {
-			$scope.app = Apps.get({
-				appId: $stateParams.appId
-			});
-		};
+        // Find existing App
+        $scope.findOne = function () {
+            $scope.app = Apps.get({
+                appId: $stateParams.appId
+            });
+        };
 
-		$scope.dtOptions = DTOptionsBuilder
-			.newOptions()
-			.withLanguage({
-				'sLengthMenu': '每页显示 _MENU_ 条数据',
-				'sInfo': '从 _START_ 到 _END_ /共 _TOTAL_ 条数据',
-				'sInfoEmpty': '没有数据',
-				'sInfoFiltered': '(从 _MAX_ 条数据中检索)',
-				'sZeroRecords': '没有检索到数据',
-				'sSearch': '检索:',
-				'oPaginate': {
-					'sFirst': '首页',
-					'sPrevious': '上一页',
-					'sNext': '下一页',
-					'sLast': '末页'
-				}
-			})
-			// Add Bootstrap compatibility
-			.withBootstrap();
+        $scope.dtOptions = DTOptionsBuilder
+            .newOptions()
+            .withLanguage({
+                'sLengthMenu': '每页显示 _MENU_ 条数据',
+                'sInfo': '从 _START_ 到 _END_ /共 _TOTAL_ 条数据',
+                'sInfoEmpty': '没有数据',
+                'sInfoFiltered': '(从 _MAX_ 条数据中检索)',
+                'sZeroRecords': '没有检索到数据',
+                'sSearch': '检索:',
+                'oPaginate': {
+                    'sFirst': '首页',
+                    'sPrevious': '上一页',
+                    'sNext': '下一页',
+                    'sLast': '末页'
+                }
+            })
+            // Add Bootstrap compatibility
+            .withBootstrap();
 
-		$scope.canUpdate = function() {
-			return $scope.appForm.$valid;
-		};
+        $scope.canUpdate = function () {
+            return $scope.appForm.$valid;
+        };
 
-		$scope.removeErr = function() {
-			$scope.error = false;
-		};
+        $scope.removeErr = function () {
+            $scope.error = false;
+        };
 
-		$scope.back = function() {
-			window.history.back();
-		};
-	}
+        $scope.back = function () {
+            window.history.back();
+        };
+    }
 ]);
