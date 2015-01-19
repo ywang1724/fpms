69e25d00fe825ad5a6774de1fb3dd83147a9a8a4
ywang1724 <kobebryant42536@gmail.com>
2015-01-19 11:07:52 +0800

    add page model

---
+++ app/models/page.server.model.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Page Schema
+ */
+var PageSchema = new Schema({
+	// Page model fields   
+	// ...
+});
+
+mongoose.model('Page', PageSchema);
