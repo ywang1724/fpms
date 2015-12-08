a1bf96d7e581a7985b5e7ad583b68e43c02492fd
front-thinking <269354528@qq.com>
2015-12-08 11:15:33 +0800

    add manual alarm route

--- app/routes/mails.server.routes.js
+++ app/routes/mails.server.routes.js
@@ -3,16 +3,23 @@
 module.exports = function(app) {
 	var users = require('../../app/controllers/users.server.controller');
 	var mails = require('../../app/controllers/mails.server.controller');
+	var exceptions = require('../../app/controllers/exceptions.server.controller');
 
 	// Mails Routes
 	app.route('/mails')
 		.get(mails.list);
+
 
 	app.route('/mails/:mailId')
 		.get(mails.read)
 		.put(users.requiresLogin, mails.hasAuthorization, mails.update)
 		.delete(users.requiresLogin, mails.hasAuthorization, mails.delete);
 
+
+	app.route('/mails/alarm/:exceptionId')
+		.put(mails.manualAlarm);
+
 	// Finish by binding the Mail middleware
 	app.param('mailId', mails.mailByID);
+	app.param('exceptionId', exceptions.exceptionByID);
 };
