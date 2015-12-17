b689b2f8945fc0af20cf63617ed5aacec3f87425
front-thinking <269354528@qq.com>
2015-12-17 15:14:20 +0800

    fix sendMail error

--- app/controllers/mails.server.controller.js
+++ app/controllers/mails.server.controller.js
@@ -32,6 +32,61 @@ var createMail = function(mailObj, user) {
 };
 
 
+
+/**
+ * 说明：发送邮件
+ * @param toAddress
+ * @param subject
+ * @param exception
+ * @param app
+ * @param page
+ */
+var sendMail = function (toAddress, subject, exception, app, page){
+	var transport = nodemailer.createTransport(config.q_mailer.options);
+	var excepType = {1: 'JavaScript异常', 2: 'Ajax请求异常', 3: '静态资源丢失异常', 4:　'死链接异常', 5: '页面加载异常', 6: 'DOM结构异常', 7: '内存异常'};
+
+	var mailMessage = '您好，您的应用 ' + app.name + ' 下的' + page.pathname + ' 发生异常，异常详情如下：<br>' +
+		'异常类型：' + excepType[exception.type] + '<br>' +
+		'异常消息：' + exception.message + '<br>' +
+		'发生时间：' + moment(exception.occurTime).format('YYYY-MM-DD HH:mm:ss') + '<br>' +
+		'浏览器：' + exception.ui.browser + '版本号' + exception.ui.version + '<br>' +
+		'平台信息：'　+　exception.ui.os + '版本号' + exception.ui.osversion + '<br>' +
+		'堆栈消息：' + exception.stack + '<br>' +
+		'请求链接：' + exception.requrl + '<br>' +
+		'异常文件：' + (exception.errorurl === '' ? '当前页面' : exception.errorurl) + '<br>' +
+		'异常页面：' + page.pathname;
+
+
+	var mailOptions = {
+		from: config.q_mailer.from, // 发件地址
+		to: toAddress, // 收件列表
+		subject: excepType[exception.type] + '报警', // 标题
+		html: mailMessage // html 内容
+	};
+
+	var mailObj = {
+		'theme': excepType[exception.type] + '报警',
+		'content': mailMessage,
+		'receiver': toAddress
+	};
+
+	transport.sendMail(mailOptions, function(error, response) {
+		if (error) {
+			console.error(error);
+			mailObj.status = 2;//设置发送失败标识位
+			createMail(mailObj, app.user);
+		} else {
+			console.log(response);
+			mailObj.status = 1;//设置发送成功标识位
+			createMail(mailObj, app.user);
+		}
+		transport.close(); // 如果没用，关闭连接池
+	});
+};
+
+//将发送邮件功能暴漏给模块外
+exports.sendMail = sendMail;
+
 /**
  * 手动报警
  */
@@ -160,53 +215,3 @@ exports.hasAuthorization = function(req, res, next) {
 };
 
 
-/**
- * 说明：发送邮件
- * @param toAddress
- * @param subject
- * @param exception
- * @param app
- * @param page
- */
-var sendMail = exports.sendMail = function (toAddress, subject, exception, app, page){
-	var transport = nodemailer.createTransport(config.q_mailer.options);
-	var excepType = {1: 'JavaScript异常', 2: 'Ajax请求异常', 3: '静态资源丢失异常', 4:　'死链接异常', 5: '页面加载异常', 6: 'DOM结构异常', 7: '内存异常'};
-
-	var mailMessage = '您好，您的应用 ' + app.name + ' 下的' + page.pathname + ' 发生异常，异常详情如下：<br>' +
-		'异常类型：' + excepType[exception.type] + '<br>' +
-		'异常消息：' + exception.message + '<br>' +
-		'发生时间：' + moment(exception.occurTime).format('YYYY-MM-DD HH:mm:ss') + '<br>' +
-		'浏览器：' + exception.ui.browser + '版本号' + exception.ui.version + '<br>' +
-		'平台信息：'　+　exception.ui.os + '版本号' + exception.ui.osversion + '<br>' +
-		'堆栈消息：' + exception.stack + '<br>' +
-		'请求链接：' + exception.requrl + '<br>' +
-		'异常文件：' + (exception.errorurl === '' ? '当前页面' : exception.errorurl) + '<br>' +
-		'异常页面：' + page.pathname;
-
-
-	var mailOptions = {
-		from: config.q_mailer.from, // 发件地址
-		to: toAddress, // 收件列表
-		subject: excepType[exception.type] + '报警', // 标题
-		html: mailMessage // html 内容
-	};
-
-	var mailObj = {
-		'theme': excepType[exception.type] + '报警',
-		'content': mailMessage,
-		'receiver': toAddress
-	};
-
-	transport.sendMail(mailOptions, function(error, response) {
-		if (error) {
-			console.error(error);
-			mailObj.status = 2;//设置发送失败标识位
-			createMail(mailObj, app.user);
-		} else {
-			console.log(response);
-			mailObj.status = 1;//设置发送成功标识位
-			createMail(mailObj, app.user);
-		}
-		transport.close(); // 如果没用，关闭连接池
-	});
-};
