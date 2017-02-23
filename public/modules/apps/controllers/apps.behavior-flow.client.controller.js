c2b5fb505d8eb9a7b59ca3c114a0ee8c8a3e94ab
tommyzqfeng <fengzheqiyx@163.com>
2017-02-23 14:20:32 +0800

    1.添加部分省份信息 2.修改highmap配置

--- public/modules/apps/controllers/apps.behavior-flow.client.controller.js
+++ public/modules/apps/controllers/apps.behavior-flow.client.controller.js
@@ -81,7 +81,7 @@ angular.module('apps').controller('AppsBehaviorFlowController', ['$scope', '$sta
       },
       {
         "hc-key": "tw-km",
-        "value": 2
+        "value": 0
       },
       {
         "hc-key": "cn-zj",
@@ -89,7 +89,7 @@ angular.module('apps').controller('AppsBehaviorFlowController', ['$scope', '$sta
       },
       {
         "hc-key": "tw-lk",
-        "value": 4
+        "value": 0
       },
       {
         "hc-key": "cn-3664",
@@ -109,259 +109,259 @@ angular.module('apps').controller('AppsBehaviorFlowController', ['$scope', '$sta
       },
       {
         "hc-key": "tw-tw",
-        "value": 9
+        "value": 0
       },
       {
         "hc-key": "tw-cs",
-        "value": 10
+        "value": 0
       },
       {
         "hc-key": "cn-6657",
-        "value": 11
+        "value": 1
       },
       {
         "hc-key": "cn-6663",
-        "value": 12
+        "value": 2
       },
       {
         "hc-key": "cn-6665",
-        "value": 13
+        "value": 1
       },
       {
         "hc-key": "cn-6666",
-        "value": 14
+        "value": 1
       },
       {
         "hc-key": "cn-6667",
-        "value": 15
+        "value": 1
       },
       {
         "hc-key": "cn-6669",
-        "value": 16
+        "value": 1
       },
       {
         "hc-key": "cn-6670",
-        "value": 17
+        "value": 1
       },
       {
         "hc-key": "cn-6671",
-        "value": 18
+        "value": 0
       },
       {
         "hc-key": "tw-kh",
-        "value": 19
+        "value": 0
       },
       {
         "hc-key": "tw-hs",
-        "value": 20
+        "value": 0
       },
       {
         "hc-key": "cn-yn",
-        "value": 21
+        "value": 0
       },
       {
         "hc-key": "cn-xz",
-        "value": 22
+        "value": 0
       },
       {
         "hc-key": "tw-hh",
-        "value": 23
+        "value": 0
       },
       {
         "hc-key": "tw-cl",
-        "value": 24
+        "value": 0
       },
       {
         "hc-key": "tw-ml",
-        "value": 25
+        "value": 0
       },
       {
         "hc-key": "cn-nx",
-        "value": 26
+        "value": 0
       },
       {
         "hc-key": "cn-sa",
-        "value": 27
+        "value": 0
       },
       {
         "hc-key": "tw-ty",
-        "value": 28
+        "value":0
       },
       {
         "hc-key": "cn-3682",
-        "value": 29
+        "value": 0
       },
       {
         "hc-key": "tw-cg",
-        "value": 30
+        "value": 0
       },
       {
         "hc-key": "cn-6655",
-        "value": 31
+        "value": 3
       },
       {
         "hc-key": "cn-ah",
-        "value": 32
+        "value": 0
       },
       {
         "hc-key": "cn-hu",
-        "value": 33
+        "value": 0
       },
       {
         "hc-key": "tw-hl",
-        "value": 34
+        "value": 0
       },
       {
         "hc-key": "tw-th",
-        "value": 35
+        "value": 0
       },
       {
         "hc-key": "cn-6656",
-        "value": 36
+        "value": 6
       },
       {
         "hc-key": "tw-nt",
-        "value": 37
+        "value": 0
       },
       {
         "hc-key": "cn-6658",
-        "value": 38
+        "value": 3
       },
       {
         "hc-key": "cn-6659",
-        "value": 39
+        "value": 3
       },
       {
         "hc-key": "cn-cq",
-        "value": 40
+        "value": 0
       },
       {
         "hc-key": "cn-hn",
-        "value": 41
+        "value": 4
       },
       {
         "hc-key": "tw-yl",
-        "value": 42
+        "value": 0
       },
       {
         "hc-key": "cn-6660",
-        "value": 43
+        "value": 3
       },
       {
         "hc-key": "cn-6661",
-        "value": 44
+        "value": 4
       },
       {
         "hc-key": "cn-6662",
-        "value": 45
+        "value": 5
       },
       {
         "hc-key": "cn-6664",
-        "value": 46
+        "value": 6
       },
       {
         "hc-key": "cn-6668",
-        "value": 47
+        "value": 7
       },
       {
         "hc-key": "tw-pt",
-        "value": 48
+        "value": 0
       },
       {
         "hc-key": "tw-tt",
-        "value": 49
+        "value": 0
       },
       {
         "hc-key": "tw-tn",
-        "value": 50
+        "value": 0
       },
       {
         "hc-key": "cn-bj",
-        "value": 51
+        "value": 34
       },
       {
         "hc-key": "cn-hb",
-        "value": 52
+        "value": 2
       },
       {
         "hc-key": "tw-il",
-        "value": 53
+        "value": 0
       },
       {
         "hc-key": "tw-tp",
-        "value": 54
+        "value": 0
       },
       {
         "hc-key": "cn-sd",
-        "value": 55
+        "value": 5
       },
       {
         "hc-key": "tw-ch",
-        "value": 56
+        "value": 0
       },
       {
         "hc-key": "cn-tj",
-        "value": 57
+        "value": 7
       },
       {
         "hc-key": "cn-ha",
-        "value": 58
+        "value": 8
       },
       {
         "hc-key": "cn-jl",
-        "value": 59
+        "value": 5
       },
       {
         "hc-key": "cn-qh",
-        "value": 60
+        "value": 0
       },
       {
         "hc-key": "cn-xj",
-        "value": 61
+        "value": 1
       },
       {
         "hc-key": "cn-nm",
-        "value": 62
+        "value": 2
       },
       {
         "hc-key": "cn-hl",
-        "value": 63
+        "value": 3
       },
       {
         "hc-key": "cn-sc",
-        "value": 64
+        "value": 4
       },
       {
         "hc-key": "cn-gz",
-        "value": 65
+        "value": 6
       },
       {
         "hc-key": "cn-gx",
-        "value": 66
+        "value": 6
       },
       {
         "hc-key": "cn-ln",
-        "value": 67
+        "value": 7
       },
       {
         "hc-key": "cn-js",
-        "value": 68
+        "value": 8
       },
       {
         "hc-key": "cn-gs",
-        "value": 69
+        "value": 6
       },
       {
         "hc-key": "cn-sx",
-        "value": 70
+        "value": 3
       },
       {
         "hc-key": "cn-he",
-        "value": 71
+        "value": 3
       },
       {
         "hc-key": "cn-jx",
-        "value": 72
+        "value": 4
       }
     ];
 
@@ -740,10 +740,14 @@ angular.module('apps').controller('AppsBehaviorFlowController', ['$scope', '$sta
       options: {
         chart:{},
         legend: {
-          enabled: false
+          layout: 'horizontal',
+          borderWidth: 0,
+          backgroundColor: 'rgba(255,255,255,0.85)',
+          floating: true,
+          verticalAlign: 'top',
+          y: 20
         },
         plotOptions: {
-
           map: {
             mapData: Highcharts.maps['countries/cn/custom/cn-all-sar-taiwan'],
             joinBy: ['hc-key'],
@@ -754,10 +758,11 @@ angular.module('apps').controller('AppsBehaviorFlowController', ['$scope', '$sta
             }
           }
         },
+        colorAxis: {},
       },
       chartType: 'map',
       title: {
-        text: 'Highcharts-ng map example'
+        text: '访问分布情况'
       },
       series: [{
         data: data,
